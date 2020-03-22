53fec54d51dba552f44bfa57875d6bdbac6c439f
adiyous <adilnewyork@gmail.com>
2020-03-22 15:24:31 -0800

    Loopin or mapping through all data and having them in divs

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -12,24 +12,26 @@ class ListAppointments extends Component {
           )
         return(
         <div className="appointment-list item-list mb-3">
-            <div className="pet-item col media py-3">
-              <div className="mr-3">
-                <button className="pet-delete btn btn-sm btn-danger">X</button>
-              </div>
-  
-              <div className="pet-info media-body">
-                <div className="pet-head d-flex">
-                  <span className="pet-name">petName</span>
-                  <span className="apt-date ml-auto">aptDate</span>
+            {this.props.appointments.map(item => (
+                <div className="pet-item col media py-3">
+                    <div className="mr-3">
+                        <button className="pet-delete btn btn-sm btn-danger">X</button>
+                    </div>
+              
+                    <div className="pet-info media-body">
+                        <div className="pet-head d-flex">
+                            <span className="pet-name">{item.petName}</span>
+                            <span className="apt-date ml-auto">{item.aptDate}</span>
+                        </div>
+              
+                        <div className="owner-name">
+                            <span className="label-item">Owner: </span>
+                            <span>{item.ownerName}</span>
+                        </div>
+                        <div className="apt-notes">{item.aptNotes}</div>
+                    </div>
                 </div>
-  
-                <div className="owner-name">
-                  <span className="label-item">Owner: </span>
-                  <span>ownerName</span>
-                </div>
-                <div className="apt-notes">aptNotes</div>
-              </div>
-            </div>
+            ))}
         </div>
         )
     }

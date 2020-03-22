2895a9686bcff12f012c909d5e40d937a082657c
adiyous <adilnewyork@gmail.com>
2020-03-22 14:03:44 -0800

    adding listItems and mapping through myAppointments

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      myAppointment: []
+      myAppointments: []
     }
   }
   componentDidMount(){
@@ -19,17 +19,29 @@ class App extends Component {
         return item
       })
       this.setState({
-        myAppointment: apts
+        myAppointments: apts
       })
     })
   }
   render(){
+    const listItems = this.state.myAppointments.map(
+      item => (
+        <div>
+          <h1>{item.petName}</h1>
+          <p>{item.aptNotes}</p>
+        </div>)
+    )
     return (
     <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
+              <ol>
+              {
+                listItems
+              }
+              </ol>
               <AddAppointments/>
               <SearchAppointments/>
               <ListAppointments />

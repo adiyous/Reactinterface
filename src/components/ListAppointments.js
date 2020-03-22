0d8dbdaa5c021ec05a5274c4befe11e1b298dd0e
adiyous <adilnewyork@gmail.com>
2020-03-22 14:21:13 -0800

    Adding delete button and pet name details in ListItems

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -11,12 +11,26 @@ class ListAppointments extends Component {
               )
           )
         return(
-            <div>
-                ListAppointments
-                {
-                    listItems
-                }
+        <div className="appointment-list item-list mb-3">
+            <div className="pet-item col media py-3">
+              <div className="mr-3">
+                <button className="pet-delete btn btn-sm btn-danger">X</button>
+              </div>
+  
+              <div className="pet-info media-body">
+                <div className="pet-head d-flex">
+                  <span className="pet-name">petName</span>
+                  <span className="apt-date ml-auto">aptDate</span>
+                </div>
+  
+                <div className="owner-name">
+                  <span className="label-item">Owner: </span>
+                  <span>ownerName</span>
+                </div>
+                <div className="apt-notes">aptNotes</div>
+              </div>
             </div>
+        </div>
         )
     }
 }

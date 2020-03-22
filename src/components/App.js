7244b98398ad37847969291f403e539ac841fdd4
adiyous <adilnewyork@gmail.com>
2020-03-22 13:39:39 -0800

    adding data.json and fetching data from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments'
 import SearchAppointments from './SearchAppointments'
 import ListAppointments from './ListAppointments'
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: 'Ali'
+    }
+  }
+  componentDidMount(){
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        return item
+      })
+    })
+  }
+  render(){
+    return (
     <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
+              <h1>{this.state.name}</h1>
+              <AddAppointments/>
+              <SearchAppointments/>
               <ListAppointments />
             </div>
           </div>
         </div>
       </div>
     </main>
-  );
+    )
+  }
 }
 
 export default App;

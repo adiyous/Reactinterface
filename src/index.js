2bfe9e79fa94fee4d72e0b7d599d6120d66eb1ed
adiyous <adilnewyork@gmail.com>
2020-03-22 10:01:37 -0800

    Organize folders, adding component and css folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'
+import './css/index.css';
+import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery.js'
+import 'popper.js/dist/umd/popper.js'
+import 'bootstrap/dist/js/bootstrap.js'
 
 ReactDOM.render(
   <React.StrictMode>
